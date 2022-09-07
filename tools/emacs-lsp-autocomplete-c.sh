b3a63a167a5c7d1234d123416992572e613b34d7
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-09-07 19:08:27 +0000

    tool: install bear dep
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tools/emacs-lsp-autocomplete-c.sh
+++ tools/emacs-lsp-autocomplete-c.sh
@@ -3,6 +3,9 @@
 # Stupid script that take a global path and
 # generate all necessary info to have the auto completion
 # on a C project that use Makefile.
+#
+# Source: https://emacs-lsp.github.io/lsp-mode/tutorials/CPP-guide/
+sudo apt-get install bear
 cd $1 | exit 1
 echo "Moved in $1"
 cd $1 && bear -vvv make -j$(nproc)
